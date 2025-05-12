e7a88a2d5ab024edee78f6f19c01d6129da698aa
MoraswiChuene03 <MoraswiChuene03>
2025-05-12 21:04:34 +0200

    Added backend page routing

--- community-portal-prefilled/routes/pageRoutes.js
+++ community-portal-prefilled/routes/pageRoutes.js
@@ -2,20 +2,36 @@
 
 const express = require('express');
 const router = express.Router();
+const events = require('../data/event');
+const team = require('../data/team');
+const {saveMessage} = require('../data/messageStorage');
+
 
 router.get('/', (req, res) => {
+    res.render('home')
 });
 
 router.get('/about', (req, res) => {
+    res.render('about', {team});
 });
 
 router.get('/events', (req, res) => {
+    res.render('events', {events});
 });
 
 router.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+router.post('/contact', (req, res) => {
+    const {name, email, message} = req.body;
+    const newMessage = { name, email, message, submittedAt: new Date().toISOString()};
+    saveMessage(newMessage);
+    res.redirect('/thankyou')
 });
 
 router.get('/thankyou', (req, res) => {
+  res.render('thankyou');
 });
 
 module.exports = router;
