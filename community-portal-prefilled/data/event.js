df3856a183f4437b5b9e55b0081f96c72865b405
Morongwa Olifant <105193242+MorongwaOlifant@users.noreply.github.com>
2025-05-13 11:51:49 +0200

    Update event.js - Images

--- community-portal-prefilled/data/event.js
+++ community-portal-prefilled/data/event.js
@@ -5,25 +5,25 @@ const events = [
     title: "TechTalks 2025",
     date: "2025-06-05",
     location: "Innovation Hub, Johannesburg",
-    image: "public/images/download.png"
+    image: "public/images/tech-talk.jpg"
   },
   {
     title: "Community Code Camp",
     date: "2025-06-18",
     location: "Cape Town Digital Centre",
-    image: "public/images/download.png"
+    image: "public/images/code-camp.jpg"
   },
   {
     title: "Youth Hackathon",
     date: "2025-07-02",
     location: "Online via Zoom",
-    image: "public/images/download.png"
+    image: "public/images/hackathon.jpg"
   },
   {
     title: "Tech for Good Expo",
     date: "2025-07-15",
     location: "Durban Convention Centre",
-    image: "public/images/download.png"
+    image: "public/images/tech-expo.jpg"
   }
 ];
 
